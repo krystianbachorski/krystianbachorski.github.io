0990b34baa9c700711b0f6fc830f13549e2f07a0
krystianbachorski <krystian2254@op.pl>
2022-07-23 21:25:17 +0200

    Add files via upload

--- forms/contact.php
+++ forms/contact.php
@@ -7,12 +7,12 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'krystian2254@op.pl';
+  $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Nie można załadować biblioteki!');
+    die( 'Unable to load the "PHP Email Form" Library!');
   }
 
   $contact = new PHP_Email_Form;
